ef16ecb78579203c1e1c5e21cd14a41b588ed87f
sandysams0n <sandy.samson@mca.christuniversity.in>
2020-12-08 13:14:04 +0530

    .5

--- l8/filter_app.js
+++ l8/filter_app.js
@@ -4,7 +4,7 @@ angular.module("MyApp",[])
     var book=[
         {title:'Pilgrim',author:'J.Bunyan',genre:"Male",year:'4.7',price:499.77},
         {title:'Alchemist',author:'P.Coelho',genre:"Male",year:'3.9',price:250},
-        {title:'Wanted',author:'A.Lynne',genre:"Female",year:'3.5',price:470},
+        {title:'Wanted',author:'A.Lynne',genre:"Female",year:'3',price:470},
         {title:'Stolen',author:'Eric L',genre:"Male",year:'4.1',price:399}
     ];
     console.log(book);
