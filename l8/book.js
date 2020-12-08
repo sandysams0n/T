774a5e483f3074edc18beb81268227c9418afff8
sandysams0n <sandy.samson@mca.christuniversity.in>
2020-12-08 14:43:10 +0530

    el

--- l8/book.js
+++ l8/book.js
@@ -1,40 +1,40 @@
 angular.module('myapp',[])
 .controller('myctrl',function($scope)
 {
-    $scope.listBook=[
-        {id:'B01',name:'Python',price:5000},
-        {id:'B02',name:'Java',price:3455},
-        {id:'B03',name:'C++',price:4205},
-        {id:'B04',name:'C programming',price:2000}
+    $scope.listProduct=[
+        {id:'P01',name:'Skirt',price:5000,piece:72},
+        {id:'P02',name:'Swearer',price:3455,piece:48},
+        {id:'P03',name:'Shirt',price:4205,piece:36},
+        {id:'P04',name:'Jeans',price:2000,piece:24}
     ];
     $scope.save=function()
     {
         var index=getindex($scope.id);
-        $scope.listBook[index].name=$scope.name;
-        $scope.listBook[index].price=$scope.price;
-        
+        $scope.listProduct[index].name=$scope.name;
+        $scope.listProduct[index].price=$scope.price;
+        $scope.listProduct[index].piece=$scope.piece;
     }
     $scope.add=function()
     {
-        $scope.listBook.push({
-            id:$scope.id,name:$scope.name,price:$scope.price
+        $scope.listProduct.push({
+            id:$scope.id,name:$scope.name,price:$scope.price,piece:$scope.piece
         });
     }
     function getindex(id)
     {
-        for(var i=0;i<$scope.listBook.length;i++)
-        if($scope.listBook[i].id==id)
+        for(var i=0;i<$scope.listProduct.length;i++)
+        if($scope.listProduct[i].id==id)
         return i;
         return -1;
     }
     $scope.edit=function(id)
     {
         var index=this.$index;
-        var course=$scope.listBook[index];
-        $scope.id=book.id;
-        $scope.name=book.name;
-        $scope.price=book.price;
-        $scope.hours=book.hours;
+        var product=$scope.listProduct[index];
+        $scope.id=product.id;
+        $scope.name=product.name;
+        $scope.price=product.price;
+        $scope.piece=product.piece;
     }
     $scope.delete=function()
     {
@@ -42,7 +42,7 @@ angular.module('myapp',[])
         if(result==true)
         {
             var index=this.$index;
-            $scope.listBook.splice(index,1);
+            $scope.listProduct.splice(index,1);
         }
     };
 });
