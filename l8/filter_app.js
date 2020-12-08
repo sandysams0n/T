15789ff12c51ec1e1ea4f2e3e03e503a86024771
sandysams0n <sandy.samson@mca.christuniversity.in>
2020-12-08 13:22:45 +0530

    td

--- l8/filter_app.js
+++ l8/filter_app.js
@@ -2,10 +2,10 @@ angular.module("MyApp",[])
 .controller("MyCtrl",function($scope)
 {
     var book=[
-        {title:'Pilgrim',author:'J.Bunyan',genre:"Male",year:'4.7',price:499.77},
-        {title:'Alchemist',author:'P.Coelho',genre:"Male",year:'3.9',price:250},
-        {title:'Wanted',author:'A.Lynne',genre:"Female",year:'3',price:470},
-        {title:'Stolen',author:'Eric L',genre:"Male",year:'4.1',price:399}
+        {title:'Pilgrim',author:'J.Bunyan',genre:"Historical",year:'4.7',price:499.77},
+        {title:'Alchemist',author:'P.Coelho',genre:"Adventure",year:'3.9',price:250},
+        {title:'Wanted',author:'A.Lynne',genre:"Fiction",year:'3',price:470},
+        {title:'Stolen',author:'Eric L',genre:"Biography",year:'4.1',price:399}
     ];
     console.log(book);
     $scope.book=book;
