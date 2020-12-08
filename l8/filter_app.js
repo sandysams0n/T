60a7ba3598d7ab0cbcedf537c3ac4952062c86a2
sandysams0n <sandy.samson@mca.christuniversity.in>
2020-12-08 12:48:18 +0530

    lab8

--- l8/filter_app.js
+++ l8/filter_app.js
@@ -1,13 +1,13 @@
 angular.module("MyApp",[])
 .controller("MyCtrl",function($scope)
 {
-    var employees=[
-        {name:'Rithul',dob:new Date("July 21,1990"),gender:"Male",salary:55000.778},
-        {name:'Indu',dob:new Date("July 20,1987"),gender:"Female",salary:65000.778},
-        {name:'Kevin',dob:new Date("November 19,1983"),gender:"Male",salary:55000.778},
-        {name:'Reena',dob:new Date("August 21,1990"),gender:"Male",salary:55000.778}
+    var book=[
+        {title:'Pilgrim',author:'J.Bunyan',genre:"Male",year:'1995' ,price:499.77},
+        {title:'Alchemist',author:'P.Coelho',genre:"Male",year:'2004' ,price:2004},
+        {title:'Wanted',author:'A.Lynne',genre:"Female",year:'2017' ,price:470},
+        {title:'Stolen',author:'Eric L',genre:"Male",year:'20`19' ,price:399}
     ];
-    console.log(employees);
-    $scope.employees=employees;
+    console.log(book);
+    $scope.book=book;
     $scope.rowlimit=4;
 });
